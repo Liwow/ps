8b4d7f06b38e8486e8f036269d40f4de5a0a28f2
李嘉俊 <88264290+Liwow@users.noreply.github.com>
2024-10-31 09:54:57 +0800

    Add files via upload

---
+++ PredictAndSearch_GRB.py
@@ -0,0 +1,276 @@
+import gurobipy
+import json
+from gurobipy import GRB
+import argparse
+
+import helper
+import test
+from test import validate_solution_in_original_model, change_model
+import random
+import os
+import numpy as np
+import torch
+from time import time
+from helper import get_a_new2
+
+
+def modify(model, n=0, k=0, fix=0):
+    # fix 0:no fix 1:随机 2:排序 3: 交集
+    if model.Status not in [GRB.OPTIMAL, GRB.TIME_LIMIT]:
+        print("No optimal solution found.")
+        return
+    slacks = {constr: constr.slack for constr in model.getConstrs() if constr.Sense in ['<', '>']}
+    filter_indices = [
+        model_to_filtered_index[i] for i in tc_1 if i in model_to_filtered_index
+    ]
+    most_tight_constraints, count_tight = test.get_most_tight_constraints(slacks, filter_indices)
+    print(f"最优解和预测投影解中松弛度都为 0 的相同约束个数: {count_tight}")
+    if fix == 0:
+        print("****** do nothing! *********")
+        return
+    sorted_slacks = sorted(slacks.items(), key=lambda item: abs(item[1]), reverse=True)
+    if n != 0:
+        most_relaxed_constraints = [constr for constr, slack in sorted_slacks[:n]]
+        print(f"Removing {n} most relaxed constraints.")
+        for constr in most_relaxed_constraints:
+            m.remove(constr)
+
+    tight_constraints = [constr for constr, slack in slacks.items() if slack == 0]
+
+    if fix == 1:
+        print("groundtruth 固定约束,固定方式:随机")
+        random.shuffle(tight_constraints)
+        most_tight_constraints = tight_constraints[:k]
+    elif fix == 2:
+        print("groundtruth 固定约束,固定方式:排序")
+        most_tight_constraints = [constr for constr, slack in sorted_slacks[-k:]] if k > 0 else []
+    elif fix == 3:
+        print("groundtruth 固定约束,固定方式:交集")
+    for constr in most_tight_constraints:
+        row = model.getRow(constr)
+        coeffs = []
+        vars = []
+        for i in range(row.size()):
+            coeffs.append(row.getCoeff(i))
+            vars.append(row.getVar(i))
+        model.remove(constr)
+        model.addConstr(gurobipy.LinExpr(coeffs, vars) == constr.RHS, name=f"{constr.ConstrName}_tight")
+
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+random.seed(0)
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+multimodal = False
+TaskName = "WA"
+if TaskName == "CA_multi":
+    multimodal = True
+# load pretrained model
+if TaskName == "IP":
+    # Add position embedding for IP model, due to the strong symmetry
+    from GCN import GNNPolicy_position as GNNPolicy, postion_get
+elif multimodal:
+    from GCN import GNNPolicy_multimodal as GNNPolicy
+else:
+    from GCN import GNNPolicy
+
+model_name = f'{TaskName}.pth'
+pathstr = f'./models/{model_name}'
+policy = GNNPolicy().to(DEVICE)
+state = torch.load(pathstr, map_location=torch.device('cuda:0'))
+policy.load_state_dict(state)
+
+# 4 public datasets, IS, WA, CA, IP
+TaskName = 'WA'
+
+
+def test_hyperparam(task):
+    '''
+    set the hyperparams
+    k_0, k_1, delta
+    '''
+    if task == "IP":
+        return 400, 5, 10
+    elif task == "IS":
+        return 300, 300, 20
+    elif task == "WA":
+        return 0, 500, 10
+    elif task == "CA" or task == "CA_m":  # 600 0 1
+        return 600, 0, 10
+    elif task == "beasley":
+        return 50, 17, 10
+    elif task == "ns":
+        return 120, 18, 20
+    elif task == "binkar":
+        return 54, 24, 10
+    elif task == "neos":
+        return 20129, 569, 700  # 20741 609
+    elif task == "mas":
+        return 136, 14, 10
+    elif task == "markshare":
+        return 14, 12, 9
+
+
+k_0, k_1, delta = test_hyperparam(TaskName)
+
+# set log folder
+solver = 'GRB'
+test_task = f'{TaskName}_{solver}_Predect&Search'
+if not os.path.isdir(f'./logs'):
+    os.mkdir(f'./logs')
+if not os.path.isdir(f'./logs/{TaskName}'):
+    os.mkdir(f'./logs/{TaskName}')
+if not os.path.isdir(f'./logs/{TaskName}/{test_task}'):
+    os.mkdir(f'./logs/{TaskName}/{test_task}')
+log_folder = f'./logs/{TaskName}/{test_task}'
+
+sample_names = sorted(os.listdir(f'./instance/test/{TaskName}'))
+count = 0
+subop_total = 0
+infeas_total = 0
+time_total = 0
+max_subop = -1
+max_infeas = -1
+max_time = 0
+
+ALL_Test = 100
+epoch = 3
+TestNum = round(ALL_Test / epoch)
+
+for e in range(epoch):
+    for ins_num in range(TestNum):
+        t_start = time()
+        test_ins_name = sample_names[(0 + e) * TestNum + ins_num]
+        ins_name_to_read = f'./instance/test/{TaskName}/{test_ins_name}'
+        # get bipartite graph as input
+        A, v_map, v_nodes, c_nodes, b_vars, _ = get_a_new2(ins_name_to_read)
+        constraint_features = c_nodes.cpu()
+        constraint_features[np.isnan(constraint_features)] = 1  # remove nan value
+        variable_features = v_nodes
+        if TaskName == "IP":
+            variable_features = postion_get(variable_features)
+        edge_indices = A._indices()
+        edge_features = A._values().unsqueeze(1)
+        edge_features = torch.ones(edge_features.shape)
+
+        # prediction
+        BD = policy(
+            constraint_features.to(DEVICE),
+            edge_indices.to(DEVICE),
+            edge_features.to(DEVICE),
+            variable_features.to(DEVICE),
+        ).sigmoid().cpu().squeeze()
+        x_pred = (BD > 0.5).float()
+
+        # align the variable name betweend the output and the solver
+        all_varname = []
+        for name in v_map:
+            all_varname.append(name)
+        binary_name = [all_varname[i] for i in b_vars]
+        scores = []  # get a list of (index, VariableName, Prob, -1, type)
+        for i in range(len(v_map)):
+            type = "C"
+            if all_varname[i] in binary_name:
+                type = 'BINARY'
+            scores.append([i, all_varname[i], BD[i].item(), -1, type])
+
+        scores.sort(key=lambda x: x[2], reverse=True)
+
+        scores = [x for x in scores if x[4] == 'BINARY']  # get binary
+
+        fixer = 0
+        # fixing variable picked by confidence scores
+        count1 = 0
+        for i in range(len(scores)):
+            if count1 < k_1:
+                scores[i][3] = 1
+                count1 += 1
+                fixer += 1
+        scores.sort(key=lambda x: x[2], reverse=False)
+        count0 = 0
+        for i in range(len(scores)):
+            if count0 < k_0:
+                scores[i][3] = 0
+                count0 += 1
+                fixer += 1
+
+        print(f'instance: {test_ins_name}, '
+              f'fix {k_0} 0s and '
+              f'fix {k_1} 1s, delta {delta}. ')
+
+        # read instance
+        m = gurobipy.read(ins_name_to_read)
+        model_to_filtered_index = helper.map_model_to_filtered_indices(m)
+        # 修复预测初始解，得到初始可行解
+        _, tc_1 = test.project_to_feasible_region_and_get_tight_constraints(m, x_pred)
+        o_m = m.copy()
+        m.Params.TimeLimit = 1000
+        m.Params.Threads = 32
+        gurobipy.setParam('LogToConsole', 1)
+
+        m.optimize()
+        obj = m.objVal
+        # obj = 23791.7
+        print("gurobi 最优解：", obj)
+        m.Params.LogFile = f'{log_folder}/{test_ins_name}.log'
+        modify(m, n=0, k=1000, fix=1)  # if fix=0  do nothing
+
+        # trust region method implemented by adding constraints
+        instance_variabels = m.getVars()
+        instance_variabels.sort(key=lambda v: v.VarName)
+        variabels_map = {}
+        for v in instance_variabels:  # get a dict (variable map), varname:var clasee
+            variabels_map[v.VarName] = v
+        alphas = []
+        for i in range(len(scores)):
+            tar_var = variabels_map[scores[i][1]]  # target variable <-- variable map
+            x_star = scores[i][3]  # 1,0,-1, decide whether need to fix
+            if x_star < 0:
+                continue
+            # tmp_var = m1.addVar(f'alp_{tar_var}', 'C')
+            tmp_var = m.addVar(name=f'alp_{tar_var}', vtype=GRB.CONTINUOUS)
+            alphas.append(tmp_var)
+            m.addConstr(tmp_var >= tar_var - x_star, name=f'alpha_up_{i}')
+            m.addConstr(tmp_var >= x_star - tar_var, name=f'alpha_dowm_{i}')
+        all_tmp = 0
+        for tmp in alphas:
+            all_tmp += tmp
+        m.addConstr(all_tmp <= delta, name="sum_alpha")
+        m.update()
+        m.optimize()
+
+        new_solution = {var.VarName: var.X for var in m.getVars()}
+        validate_solution_in_original_model(o_m, new_solution)
+
+        t = time() - t_start
+        time_total += t
+        if max_time <= t:
+            max_time = t
+        if m.status == GRB.OPTIMAL:
+            pre_obj = m.objVal
+            subop = abs(pre_obj - obj) / abs(obj)
+            subop_total += subop
+            if subop > max_subop:
+                max_subop = subop
+            if subop < 1e-4:
+                count += 1
+            print(f"ps 最优值：{m.objVal}; subopt: {round(subop, 6)}")
+        else:
+            print("不可行")
+
+total_num = TestNum * epoch
+results = {
+    "acc": count / total_num,
+    "avg_subopt": round(subop_total / total_num, 6),
+    "max_subopt": round(max_subop, 6),
+    "mean_time_pred": round(time_total / total_num, 6),
+    "max_time": round(max_time, 6)
+}
+results_dir = f"/home/ljj/project/predict_and_search/results/{TaskName}/"
+if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
+with open(results_dir + "results.json", "w") as file:
+    json.dump(results, file)
+print("acc: ", results['acc'])
+print("avg_subopt： ", results['avg_subopt'])
+print("max_subopt: ", results['max_subopt'])
---
+++ trainPredictModel.py
@@ -0,0 +1,210 @@
+import os
+import warnings
+from transformers import T5Tokenizer, T5EncoderModel
+import torch
+import torch_geometric
+import random
+import time
+import warnings
+from torch.optim.lr_scheduler import LambdaLR
+from tqdm import tqdm
+
+import utils
+
+torch.backends.cudnn.enabled = True
+torch.backends.cudnn.benchmark = True
+# this file is to train a predict model. given a instance's bipartite graph as input, the model predict the binary distribution.
+
+# 4 public datasets, IS, WA, CA, IP
+# train task
+TaskName = "CA_m"
+multimodal = True
+warnings.filterwarnings("ignore")
+# set folder
+train_task = f'{TaskName}_train'
+if not os.path.isdir(f'./train_logs'):
+    os.mkdir(f'./train_logs')
+if not os.path.isdir(f'./train_logs/{train_task}'):
+    os.mkdir(f'./train_logs/{train_task}')
+if not os.path.isdir(f'./pretrain'):
+    os.mkdir(f'./pretrain')
+if not os.path.isdir(f'./pretrain/{train_task}'):
+    os.mkdir(f'./pretrain/{train_task}')
+model_save_path = f'./pretrain/{train_task}/'
+log_save_path = f"train_logs/{train_task}/"
+log_file = open(f'{log_save_path}{train_task}_train.log', 'wb')
+
+# set params
+LEARNING_RATE = 0.001
+NB_EPOCHS = 300
+BATCH_SIZE = 8
+NUM_WORKERS = 0
+WEIGHT_NORM = 100
+
+# dataset task
+TaskName = "CA_m"
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DIR_BG = f'./dataset/{TaskName}/BG'
+DIR_SOL = f'./dataset/{TaskName}/solution'
+sample_names = os.listdir(DIR_BG)
+sample_files = [(os.path.join(DIR_BG, name), os.path.join(DIR_SOL, name).replace('bg', 'sol')) for name in sample_names]
+train_files, valid_files = utils.split_sample_by_blocks(sample_files, 0.8, block_size=100)
+
+if TaskName == "IP":
+    # Add position embedding for IP model, due to the strong symmetry
+    from GCN import GNNPolicy_position as GNNPolicy
+    from GCN import GraphDataset_position as GraphDataset
+elif multimodal:
+    from GCN import GraphDataset
+    from GCN import GNNPolicy_multimodal as GNNPolicy
+else:
+    from GCN import GraphDataset, GNNPolicy
+
+train_data = GraphDataset(train_files)
+train_loader = torch_geometric.loader.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True,
+                                                 num_workers=NUM_WORKERS)
+valid_data = GraphDataset(valid_files)
+valid_loader = torch_geometric.loader.DataLoader(valid_data, batch_size=BATCH_SIZE, shuffle=False,
+                                                 num_workers=NUM_WORKERS)
+
+PredictModel = GNNPolicy(TaskName).to(DEVICE)
+
+
+def lr_lambda(epoch):
+    return 0.95 ** ((epoch + 1) // 20)
+
+
+def EnergyWeightNorm(task):
+    if task == "IP":
+        return 1
+    elif task == "WA":
+        return 100
+    elif task == "CA" or task == "CA_m" or task == "CA_multi":
+        return -4000
+    elif task == "beasley":
+        return 100
+    elif task == "binkar":
+        return 1000
+    elif task == "ns":
+        return 10000
+    elif task == "neos":
+        return 100
+    elif task == "mas":
+        return 10000
+    elif task == "markshare":
+        return 10
+
+
+def train(predict, data_loader, epoch, optimizer=None, weight_norm=1):
+    """
+    This function will process a whole epoch of training or validation, depending on whether an optimizer is provided.
+    """
+
+    if optimizer:
+        predict.train()
+        optimizer.zero_grad()
+        desc = "Train "
+    else:
+        predict.eval()
+        desc = "Valid "
+    mean_loss = 0
+    n_samples_processed = 0
+    with torch.set_grad_enabled(optimizer is not None):
+        for step, batch in enumerate(tqdm(data_loader, desc=f"{desc}Epoch {epoch}")):
+
+            batch = batch.to(DEVICE)
+            accumulation_steps = 16
+            # get target solutions in list format
+            solInd = batch.nsols
+            target_sols = []
+            target_vals = []
+            solEndInd = 0
+            valEndInd = 0
+
+            for i in range(solInd.shape[0]):  # for in batch
+                nvar = len(batch.varInds[i][0][0])
+                solStartInd = solEndInd
+                solEndInd = solInd[i] * nvar + solStartInd
+                valStartInd = valEndInd
+                valEndInd = valEndInd + solInd[i]
+                sols = batch.solutions[solStartInd:solEndInd].reshape(-1, nvar)
+                vals = batch.objVals[valStartInd:valEndInd]
+
+                target_sols.append(sols)
+                target_vals.append(vals)
+
+            # Compute the logits (i.e. pre-softmax activations) according to the policy on the concatenated graphs
+            batch.constraint_features[torch.isinf(batch.constraint_features)] = 10  # remove nan value
+            # predict the binary distribution, BD
+            BD = predict(
+                batch.constraint_features,
+                batch.edge_index,
+                batch.edge_attr,
+                batch.variable_features,
+            )
+            BD = BD.sigmoid()
+
+            # compute loss
+            loss = 0
+            # calculate weights
+            index_arrow = 0
+            # print("start calculate loss  :")
+            for ind, (sols, vals) in enumerate(zip(target_sols, target_vals)):
+                # compute weight
+                n_vals = vals
+                exp_weight = torch.exp(-n_vals / weight_norm)
+                weight = exp_weight / exp_weight.sum()
+
+                # get a binary mask
+                varInds = batch.varInds[ind]
+                varname_map = varInds[0][0]
+                b_vars = varInds[1][0].long()
+
+                # get binary variables
+                sols = sols[:, varname_map][:, b_vars]
+
+                # cross-entropy
+                n_var = batch.ntvars[ind]
+                pre_sols = BD[index_arrow:index_arrow + n_var].squeeze()[b_vars]
+                index_arrow = index_arrow + n_var
+                pos_loss = -(pre_sols + 1e-8).log()[None, :] * (sols == 1).float()
+                neg_loss = -(1 - pre_sols + 1e-8).log()[None, :] * (sols == 0).float()
+                sum_loss = pos_loss + neg_loss
+
+                sample_loss = sum_loss * weight[:, None]
+                loss += sample_loss.sum()
+            if optimizer is not None:
+                loss.backward()
+            if optimizer is not None and (step + 1) % accumulation_steps == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+            if optimizer is not None and step == len(data_loader) - 1:
+                optimizer.step()
+                optimizer.zero_grad()
+            mean_loss += loss.item()
+            n_samples_processed += batch.num_graphs
+    mean_loss /= n_samples_processed
+
+    return mean_loss
+
+
+optimizer = torch.optim.Adam(PredictModel.parameters(), lr=LEARNING_RATE)
+scheduler = LambdaLR(optimizer, lr_lambda)
+weight_norm = EnergyWeightNorm(TaskName) if not None else 100
+best_val_loss = 99999
+for epoch in range(NB_EPOCHS):
+
+    begin = time.time()
+    train_loss = train(PredictModel, train_loader, epoch, optimizer, weight_norm)
+    scheduler.step()
+    print(f"Epoch {epoch} Train loss: {train_loss:0.3f}")
+    valid_loss = train(PredictModel, valid_loader, epoch, None, weight_norm)
+    print(f"Epoch {epoch} Valid loss: {valid_loss:0.3f}")
+    if valid_loss < best_val_loss:
+        best_val_loss = valid_loss
+        torch.save(PredictModel.state_dict(), model_save_path + 'model_best.pth')
+    torch.save(PredictModel.state_dict(), model_save_path + 'model_last.pth')
+    st = f'@epoch{epoch}   Train loss:{train_loss}   Valid loss:{valid_loss}    TIME:{time.time() - begin}\n'
+    log_file.write(st.encode())
+    log_file.flush()
+print('done')
